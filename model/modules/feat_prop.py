13ac0b743b899f53c70f08e0ac3c4d7e999a08ef
Rafael Silva <rafael@rafael.ai>
2024-02-05 08:19:44 -0500

    converts MMCV ConvModule to torch.Conv2d

--- model/modules/feat_prop.py
+++ model/modules/feat_prop.py
@@ -1,22 +1,55 @@
 """
-    BasicVSR++: Improving Video Super-Resolution with Enhanced Propagation and Alignment, CVPR 2022
+    BasicVSR++
+    Improving Video Super-Resolution withEnhanced Propagation and Alignment, CVPR 2022
 """
+
+import math
 import torch
 import torch.nn as nn
 import torchvision
-
-from mmcv.ops import ModulatedDeformConv2d, modulated_deform_conv2d
-from mmcv.cnn import constant_init
-
+from typing import Tuple, Union
 from model.modules.flow_comp import flow_warp
 
 
-class SecondOrderDeformableAlignment(ModulatedDeformConv2d):
+class SecondOrderDeformableAlignment(nn.Module):
     """Second-order deformable alignment module."""
-    def __init__(self, *args, **kwargs):
-        self.max_residue_magnitude = kwargs.pop('max_residue_magnitude', 10)
 
-        super(SecondOrderDeformableAlignment, self).__init__(*args, **kwargs)
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, Tuple[int]],
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1,
+        groups: int = 1,
+        deform_groups: int = 1,
+        bias: Union[bool, str] = True,
+    ):
+
+        self.max_residue_magnitude: int = 10
+
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = (kernel_size, kernel_size)
+        self.stride = (stride, stride)
+        self.padding = (padding, padding)
+        self.dilation = (dilation, dilation)
+        self.groups = groups
+        self.deform_groups = deform_groups
+        self.transposed = False
+        self.output_padding = 0
+
+        self.weight = nn.Parameter(
+            torch.Tensor(out_channels, in_channels // groups, *self.kernel_size)
+        )
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter("bias", None)
+        self.init_weights()
 
         self.conv_offset = nn.Sequential(
             nn.Conv2d(3 * self.out_channels + 4, self.out_channels, 3, 1, 1),
@@ -30,8 +63,23 @@ class SecondOrderDeformableAlignment(ModulatedDeformConv2d):
 
         self.init_offset()
 
+    def init_weights(self):
+        n = self.in_channels
+        for k in self.kernel_size:
+            n *= k
+        stdv = 1.0 / math.sqrt(n)
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.zero_()
+
     def init_offset(self):
-        constant_init(self.conv_offset[-1], val=0, bias=0)
+        module = self.conv_offset[-1]
+        val = 0
+        bias = 0
+        if hasattr(module, "weight") and module.weight is not None:
+            nn.init.constant_(module.weight, val)
+        if hasattr(module, "bias") and module.bias is not None:
+            nn.init.constant_(module.bias, bias)
 
     def forward(self, x, extra_feat, flow_1, flow_2):
         extra_feat = torch.cat([extra_feat, flow_1, flow_2], dim=1)
@@ -39,15 +87,10 @@ class SecondOrderDeformableAlignment(ModulatedDeformConv2d):
         o1, o2, mask = torch.chunk(out, 3, dim=1)
 
         # offset
-        offset = self.max_residue_magnitude * torch.tanh(
-            torch.cat((o1, o2), dim=1))
+        offset = self.max_residue_magnitude * torch.tanh(torch.cat((o1, o2), dim=1))
         offset_1, offset_2 = torch.chunk(offset, 2, dim=1)
-        offset_1 = offset_1 + flow_1.flip(1).repeat(1,
-                                                    offset_1.size(1) // 2, 1,
-                                                    1)
-        offset_2 = offset_2 + flow_2.flip(1).repeat(1,
-                                                    offset_2.size(1) // 2, 1,
-                                                    1)
+        offset_1 = offset_1 + flow_1.flip(1).repeat(1, offset_1.size(1) // 2, 1, 1)
+        offset_2 = offset_2 + flow_2.flip(1).repeat(1, offset_2.size(1) // 2, 1, 1)
         offset = torch.cat([offset_1, offset_2], dim=1)
 
         # mask
@@ -57,25 +100,26 @@ class SecondOrderDeformableAlignment(ModulatedDeformConv2d):
             x,
             offset,
             self.weight,
-            self.bias, 
+            self.bias,
             self.stride,
             self.padding,
             self.dilation,
             mask,
-            )
+        )
 
 
 class BidirectionalPropagation(nn.Module):
     def __init__(self, channel):
         super(BidirectionalPropagation, self).__init__()
-        modules = ['backward_', 'forward_']
+        modules = ["backward_", "forward_"]
         self.deform_align = nn.ModuleDict()
         self.backbone = nn.ModuleDict()
         self.channel = channel
 
         for i, module in enumerate(modules):
             self.deform_align[module] = SecondOrderDeformableAlignment(
-                2 * channel, channel, 3, padding=1, deform_groups=16)
+                2 * channel, channel, 3, padding=1, deform_groups=16
+            )
 
             self.backbone[module] = nn.Sequential(
                 nn.Conv2d((2 + i) * channel, channel, 3, 1, 1),
@@ -92,18 +136,18 @@ class BidirectionalPropagation(nn.Module):
         """
         b, t, c, h, w = x.shape
         feats = {}
-        feats = {'spatial': [x[:, i, :, :, :] for i in range(t)]}
+        feats = {"spatial": [x[:, i, :, :, :] for i in range(t)]}
 
         for module_name, module in self.deform_align.items():
 
             feats[module_name] = []
 
             flow_idx = list(range(-1, t - 1))
-            mapping_idx = list(range(0, len(feats['spatial'])))
+            mapping_idx = list(range(0, len(feats["spatial"])))
             mapping_idx += mapping_idx[::-1]
 
-            if 'backward' in module_name:
-                frame_idx = list(range(t-1, -1, -1))
+            if "backward" in module_name:
+                frame_idx = list(range(t - 1, -1, -1))
                 flows = flows_backward
             else:
                 frame_idx = list(range(0, t))
@@ -111,7 +155,7 @@ class BidirectionalPropagation(nn.Module):
 
             feat_prop = x.new_zeros(b, self.channel, h, w)
             for i, idx in enumerate(frame_idx):
-                feat_current = feats['spatial'][mapping_idx[idx]]
+                feat_current = feats["spatial"][mapping_idx[idx]]
 
                 if i > 0:
                     flow_n1 = flows[:, flow_idx[i], :, :, :]
@@ -125,38 +169,36 @@ class BidirectionalPropagation(nn.Module):
                         feat_n2 = feats[module_name][-2]
                         flow_n2 = flows[:, flow_idx[i - 1], :, :, :]
                         flow_n2 = flow_n1 + flow_warp(
-                            flow_n2, flow_n1.permute(0, 2, 3, 1))
-                        cond_n2 = flow_warp(feat_n2,
-                                            flow_n2.permute(0, 2, 3, 1))
+                            flow_n2, flow_n1.permute(0, 2, 3, 1)
+                        )
+                        cond_n2 = flow_warp(feat_n2, flow_n2.permute(0, 2, 3, 1))
 
                     cond = torch.cat([cond_n1, feat_current, cond_n2], dim=1)
                     feat_prop = torch.cat([feat_prop, feat_n2], dim=1)
-                    feat_prop = module(feat_prop, cond,
-                                                               flow_n1,
-                                                               flow_n2)
+                    feat_prop = module(feat_prop, cond, flow_n1, flow_n2)
 
                 feat = []
                 feat.append(feat_current)
                 for k in feats:
-                    if k not in ['spatial', module_name]:
+                    if k not in ["spatial", module_name]:
                         feat.append(feats[k][idx])
                 feat.append(feat_prop)
 
                 feat = torch.cat(feat, dim=1)
-                if module_name == 'backward_':
+                if module_name == "backward_":
                     feat_prop = feat_prop + self.backbone["backward_"](feat)
                 else:
                     feat_prop = feat_prop + self.backbone["forward_"](feat)
                 feats[module_name].append(feat_prop)
 
-            if 'backward' in module_name:
+            if "backward" in module_name:
                 feats[module_name].reverse()
 
         outputs = []
         for i in range(0, t):
             align_feats = []
             for k in feats:
-                if k != 'spatial':
+                if k != "spatial":
                     align_feats.append(feats[k].pop(0))
             align_feats = torch.cat(align_feats, dim=1)
             outputs.append(self.fusion(align_feats))
